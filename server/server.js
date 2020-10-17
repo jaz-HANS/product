5481c55acfecc08b0009e29c3fe236ab7ae02490
John Doolittle <jdoo.code@gmail.com>
2020-10-17 13:57:14 -0600

    Parser setup

--- server/server.js
+++ server/server.js
@@ -1,11 +1,19 @@
 /* eslint-disable no-console */
 const express = require('express');
+const parser = require('body-parser');
+const db = require('./db');
 
 const app = express();
+module.exports.app = app;
 const PORT = 3001;
+
+app.use(parser.json());
+app.use(parser.urlencoded({ extended: true }));
 
 app.use(express.static('../client/dist'));
 
 app.listen(PORT, () => {
   console.log(`Server running and listening on port: ${PORT}`);
 });
+
+module.exports = app;
