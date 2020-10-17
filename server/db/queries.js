6872715338c691fd1860ff6488ec5d991830bb14
John Doolittle <jdoo.code@gmail.com>
2020-10-17 13:44:33 -0600

    Created db folder, moved mongoose connection to queries.js

---
+++ server/db/queries.js
@@ -0,0 +1,7 @@
+const mongoose = require('mongoose');
+
+const mongoDB = 'mongodb://127.0.0.1/sdc';
+mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
--- server/schema.js
+++ server/schema.js
@@ -1,11 +1,5 @@
 const mongoose = require('mongoose');
 
-const mongoDB = 'mongodb://127.0.0.1/sdc';
-mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
-
-const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
-
 const Schema = mongoose.Schema;
 
 const ProductSchema = new Schema({
