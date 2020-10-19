340e663dee01cddba0fdcd25bf7e106f9ae5a758
John Doolittle <jdoo.code@gmail.com>
2020-10-19 14:47:07 -0600

    Implemented cart/getProductList

--- server/db/controllers/cart.js
+++ server/db/controllers/cart.js
@@ -1,6 +1,15 @@
 const { Cart } = require('../models/cart');
 
 const CartController = {
+  getProductList: (session, callback) => {
+    Cart.find({ user_session: session }, (err, docs) => {
+      if (err) {
+        callback(err);
+      } else {
+        callback(null, docs);
+      }
+    });
+  },
 };
 
 module.exports = {
